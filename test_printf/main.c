92c915225512e06ec5ac4b44fc53208ca53ae51c
Romain LECART <rlecart@e3r9p20.42.fr>
2017-06-20 00:56:37 +0200

    [rlecart] - New printf test main.

--- test_printf/main.c
+++ test_printf/main.c
@@ -6,17 +6,22 @@
 /*   By: rlecart <rlecart@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/01 16:01:40 by rlecart           #+#    #+#             */
-/*   Updated: 2017/06/14 04:39:01 by rlecart          ###   ########.fr       */
+/*   Updated: 2017/06/15 14:26:56 by rlecart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../ft_printf/includes/ft_printf.h"
+#include <limits.h>
 
 int				main(void)
 {
 	int		ret;
 
-	ret = 42;
-	ret = ft_printf("%s %p %d %bonjour\n", "bonjour", ret, ret);
+//	ret = ft_printf("%D\n", LONG_MAX);
+//	ret = ft_printf("%D\n", LONG_MIN);
+	ret = printf("% Zoooo");
+	printf("\n%d\n", ret);
+	ret = ft_printf("% Zoooo");
+	printf("\n%d\n", ret);
 	return (0);
 }
